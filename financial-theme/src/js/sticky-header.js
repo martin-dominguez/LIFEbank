e26739ac231798f4c31724e0e9bb15e97ca0cf7b
martin-dominguez <martin.dominguez@liferay.com>
2020-04-07 09:24:25 +0200

    Fixed sticky header issues

--- financial-theme/src/js/sticky-header.js
+++ financial-theme/src/js/sticky-header.js
@@ -3,8 +3,10 @@ AUI().ready('liferay-sign-in-modal', 'event-outside', 'transition', function(A) 
     var fix_menu = A.one('.fix-menu');
     var portlet_login = A.one('.portlet-login');
     var portlet_status = A.one('#portlet_status');
+    var portlet_notifications = A.one(".notifications-portlet");
+    var portlet_my_account = A.one("#portlet_com_liferay_my_account_web_portlet_MyAccountPortlet")
     
-    if ((portlet_login || portlet_status) && (fix_menu)) {
+    if ((portlet_login || portlet_status || portlet_notifications || portlet_my_account) && (fix_menu)) {
         A.one('.navbar-area').addClass('is-sticky');
     } else {
         // **** ACTIONS ON SCROLL ****
