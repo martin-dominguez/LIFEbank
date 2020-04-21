234a333b38ed009264c8081cbfba4de72a00abdd
martin-dominguez <martin.dominguez@liferay.com>
2020-04-21 10:47:58 +0200

    Sticky header fixes for editing mode

--- financial-theme/src/js/sticky-header.js
+++ financial-theme/src/js/sticky-header.js
@@ -4,9 +4,11 @@ AUI().ready('liferay-sign-in-modal', 'event-outside', 'transition', function(A) 
     var portlet_login = A.one('.portlet-login');
     var portlet_status = A.one('#portlet_status');
     var portlet_notifications = A.one(".notifications-portlet");
-    var portlet_my_account = A.one("#portlet_com_liferay_my_account_web_portlet_MyAccountPortlet")
+    var portlet_my_account = A.one("#portlet_com_liferay_my_account_web_portlet_MyAccountPortlet");
+    var portlet_workflow = A.one("#_com_liferay_portal_workflow_task_web_portlet_MyWorkflowTaskPortlet_form");
+    var portlet_workflow_tasks = A.one(".portlet-workflow-tasks");
     
-    if ((portlet_login || portlet_status || portlet_notifications || portlet_my_account) && (fix_menu)) {
+    if ((portlet_login || portlet_status || portlet_notifications || portlet_my_account || portlet_workflow || portlet_workflow_tasks) && (fix_menu)) {
         A.one('.navbar-area').addClass('is-sticky');
     } else {
         // **** ACTIONS ON SCROLL ****
